f2c7cceaa411a7157f77e8dc95c2bc79f8155abc
isaiahpng <100270360+isaiahpng@users.noreply.github.com>
2024-09-18 14:18:24 -0500

    Add files via upload

--- color-picker/index.js
+++ color-picker/index.js
@@ -11,10 +11,13 @@
     - 
 
 */
+let colorNum = 3;
 let hexCode;
 let hex;
 let hexLetters = "0123456789ABCDEF";
 const colors = ["color-one", "color-two", "color-three", "color-four"];
+let comp;
+let white = "#FFFFFF"
 
 function newHex() {
     hexCode = "#";    
@@ -25,14 +28,49 @@ function newHex() {
     return(hexCode);   
 }
 
-function newPalette() {
-    for (let i = 0; i < 4; i++) {
-        hex = newHex();
-        document.getElementById(colors[i]).innerText = hex;
-        document.getElementById(colors[i]).style.backgroundColor = hex;
-    }
+function hexToRgb(h) {
+    return['0x'+h[1]+h[2]|0,'0x'+h[3]+h[4]|0,'0x'+h[5]+h[6]|0]
 }
 
-function changeSvg() {
-    document.getElementById("unlock").style.color = "green";
+function rgbToHex(r,g,b) {
+    return"#"+((1<<24)+(r<<16)+(g<<8)+ b).toString(16).slice(1);
 }
+
+function avgHex(h1,h2) {
+    a=hexToRgb(h1);
+    b=hexToRgb(h2); 
+    
+    return rgbToHex(~~((a[0]+b[0])/2),~~((a[1]+b[1])/2),~~((a[2]+b[2])/2));
+}
+
+function newPalette() {
+    hex = newHex();
+    document.getElementById("color-one").style.backgroundColor = hex;
+    document.getElementById("color-one-input").value = hex;
+    document.getElementById("color-one-input").style.backgroundColor = hex;
+}
+
+var form = document.getElementById("myForm");
+
+function manualChange(event) {
+    let input;
+    input = "#123123";
+    document.getElementById("#color-one-input").value = "hello"
+    document.getElementById("color-one").style.backgroundColor = input;
+    document.getElementById("color-one-input").value = input;
+    event.preventDefault();
+}
+
+form.addEventListener('Enter', manualChange);
+
+
+function addColor() {
+    console.log("color added");
+}
+
+function delColor() {
+    console.log("color deleted");
+
+
+}
+
